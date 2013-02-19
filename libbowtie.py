8f76fedc6da3cdbcf8b610212a2de953f360d441
Ali Ebrahim <aebrahim@ucsd.edu>
2013-02-19 13:38:39 -0800

    add are_sequences=True/False option
    This will align raw sequences

--- libbowtie.py
+++ libbowtie.py
@@ -11,14 +11,16 @@ except OSError:
     _libbowtie = ctypes.CDLL("libbowtie2.so")
 _bowtie_c_func = _libbowtie.bowtie
 
+index_location = os.path.join(os.path.dirname(os.path.abspath(__file__)), "indexes", "")
+
 def bowtie(index, outfile_name, unpaired=None, m1=None, m2=None,
-           verbose=True, preset=None, threads=None, extra_arguments=[]):
+           verbose=True, preset=None, threads=None,
+           are_sequences=False,
+           extra_arguments=[]):
     if unpaired is None and m1 is None and m2 is None:
         raise TypeError("unpaired, m1, and m2 cannot all be none")
     # create list of arguments
     arguments = ["python_bowtie_wrapper"]
-    # optional arguments
-    arguments.extend(extra_arguments)
     if preset is not None:
         if not preset.startswith("--"):
             preset = "--" + preset
@@ -26,19 +28,33 @@ def bowtie(index, outfile_name, unpaired=None, m1=None, m2=None,
     if threads is not None:
         arguments.extend(["--threads", str(threads)])
     # index and input fastq arguments
+    if not os.path.isfile(index + ".1.bt2"):
+        if os.path.isfile(index_location + index + ".1.bt2"):
+            index = index_location + index
+        else:
+            raise IOError("index %s not found" % index)
     arguments.extend(["-x", index])
     if unpaired is not None:
-        if not os.path.isfile(unpaired):
-            raise IOError("File %s not found" % unpaired)
+        if not are_sequences:
+            if not os.path.isfile(unpaired):
+                raise IOError("File %s not found" % unpaired)
         arguments.extend(["-U", unpaired])
     else:
-        if not os.path.isfile(m1):
-            raise IOError("File %s not found" % m1)
-        if not os.path.isfile(m2):
-            raise IOError("File %s not found" % m2)    
+        if not are_sequences:
+            if not os.path.isfile(m1):
+                raise IOError("File %s not found" % m1)
+            if not os.path.isfile(m2):
+                raise IOError("File %s not found" % m2)    
         arguments.extend(["-1", m1, "-2", m2])
     # output argument
     arguments.extend(["-S", outfile_name])
+    
+    if are_sequences:
+        arguments.append("-c")
+        
+    # optional arguments
+    arguments.extend(extra_arguments)
+    print arguments
 
     # create ctypes argv
     argc = len(arguments)
